38aa1525ef28436770cdbce3ba276bac0d7db6a4
Lum <LumKitty@users.noreply.github.com>
2024-03-18 19:02:26 +0000

    A shit script to search VNyan nodes

---
+++ Search-VNyan.ps1
@@ -0,0 +1,24 @@
+﻿# A shit script to search VNyan nodes
+# https://twitch.tv/LumKitty
+
+Param(
+    [Parameter(Mandatory, Position=1)] $SearchTerm
+)
+
+$Nodes = gci "$ENV:UserProfile\appdata\locallow\Suvidriel\VNyan\redeems*.json"
+ForEach ($Node in $Nodes) {
+    $Content = Get-Content $Node | ConvertFrom-Json
+    $Results = @()
+    ForEach ($Node in $Content.nodes) {
+        $Success = $False
+        ForEach ($Value in $Node.Values) {
+            if ($Value.value -like $SearchTerm) { $Success = $True }
+        }
+        if ($Success) { $Results += $Node }
+    }
+
+    if ($Results) {
+        Write-Host "Node Graph name: $($Content.GraphName)"
+        $Results | Select values, posX, PosY, path | FL
+    }
+}
