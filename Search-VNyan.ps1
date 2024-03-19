ca5d73159e48f19c3ada4e2c98f74605201d916e
Lum <LumKitty@users.noreply.github.com>
2024-03-19 17:01:06 +0000

    A shit script to search VNyan nodes

--- Search-VNyan.ps1
+++ Search-VNyan.ps1
@@ -1,9 +1,15 @@
 ﻿# A shit script to search VNyan nodes
-# https://twitch.tv/LumKitty
+#
+# https://twitch.tv/LumKitty - https://github.com/LumKitty
+#
+# Changelog
+# v1.1 - Prettier, more readable output
 
 Param(
     [Parameter(Mandatory, Position=1)] $SearchTerm
 )
+
+$FinalResults = @()
 
 $Nodes = gci "$ENV:UserProfile\appdata\locallow\Suvidriel\VNyan\redeems*.json"
 ForEach ($Node in $Nodes) {
@@ -17,8 +23,14 @@ ForEach ($Node in $Nodes) {
         if ($Success) { $Results += $Node }
     }
 
-    if ($Results) {
-        Write-Host "Node Graph name: $($Content.GraphName)"
-        $Results | Select values, posX, PosY, path | FL
+    ForEach ($Result in $Results) {
+        $FinalResults += [PSCustomObject]@{
+            Graph = $Content.GraphName
+            Name  = $Result.values | Where value -like $SearchTerm | Select -ExpandProperty value
+            Type  = $Result.Path.Replace('Nodes/','')
+            X     = [Math]::Round($Result.posX)
+            Y     = [Math]::Round($Result.posY)
+        }
     }
 }
+$FinalResults | FT
