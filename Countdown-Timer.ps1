0af5fda594df0308a8c9eeeb03cfba167cf5e340
Lum <LumKitty@users.noreply.github.com>
2024-08-25 19:54:12 +0100

    v1.2
    Added -Past parameter. You can now specify that e.g. your stream will be starting at half past the current hour (or next hour if it's already e.g. 6:40). Example:
    The time is currently 4:20
    .\Countdown-Timer.ps1 30 -Past   # Script will count down for 10 minutes and be done at 4:30
    .\Countdown-Timer.ps1 15 -Past   # Script will count down for 55 minutes and be done at 5:15
    .\Countdown-Timer.ps1 0 -Past     # Script will count down for 40 minutes and be done at 5:00
    
    Optimised the calculations for writing the output file into a oneliner. A little bit less readable but should be faster and more efficient
    Calculate the delay between writes using the exact current time, and also in a one-liner. Again less readable but more efficient. This should fix any skipped seconds in the countdown

--- Countdown-Timer.ps1
+++ Countdown-Timer.ps1
@@ -1,4 +1,4 @@
-﻿# Stream start countdown timer v1.1 - by Lum 
+﻿# Stream start countdown timer v1.2 - by Lum 
 # https://twitch.tv/LumKitty https://github.com/LumKitty
 
 # Usage
@@ -11,6 +11,10 @@
 # Stream is starting in 10 minutes
 # .\Countdown-Timer.ps1 -Minutes 10
 
+# Stream is starting at half past the current hour (e.g. run this at 6:10 and it will start at 6:30 giving a 20min countdown)
+# .\Countdown-Timer.ps1 -Minutes 30 -Past
+
+
 # Streamdeck config using BarRaider's Advanced Launcher
 # Application   : C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
 # Arguments     : -WindowStyle Minimized D:\Twitch\Scripts\Countdown-Timer.ps1 -TrgTime 18:00
@@ -20,11 +24,28 @@
 # Indicator     : no
 # Background    : no
 
+# Changelog
+
+# v1.1 - Fix killing of previous versions of the timer
+# v1.2 - Add -Past parameter, allows starting the script with e.g. 30 -Past to start at 30 minutes past the current hour (or next hour if it's already half past)
+#        More efficient (but less readable) calculation of remaining time and delay times in the countdown loop to (hopefully) eliminate skipped seconds
+
 [CmdletBinding()]
 Param (
-    [Parameter(Mandatory, Position=1, ParameterSetName='Target')] [DateTime] $TrgTime     # Countdown until this specified time
-   ,[Parameter(Mandatory, Position=1, ParameterSetName='Time')]   [Int]      $Minutes     # Counddown for this many minutes
-   ,[Parameter(           Position=2, ParameterSetName='Time')]   [Int]      $Seconds = 0 # And this many seconds
+    [Parameter(Mandatory, Position=1, ParameterSetName='Target')] 
+    [String]   $TrgTime     # Countdown until this specified time
+
+   ,[Parameter(Mandatory, Position=1, ParameterSetName='Time')]
+    [Parameter(Mandatory, Position=1, ParameterSetName='xPast')]
+    [Int]      $Minutes     # Counddown for this many minutes
+   
+   ,[Parameter(           Position=2, ParameterSetName='Time')]
+    [Parameter(           Position=2, ParameterSetName='xPast')]
+    [Int]      $Seconds = 0 # And this many seconds
+
+   ,[Parameter(Mandatory,             ParameterSetName='xPast')]
+    [Switch]   $Past
+
    ,[String] $Prefix = '' # A text string to output before the text
    ,[String] $Suffix = '' # A text string to output after the text
    ,[String] $Format = 'mm\:ss' # Format to display the time in
@@ -34,8 +55,15 @@ Param (
 
 $ErrorActionPreference = 'Stop'
 
-If ($PSCmdlet.ParameterSetName -eq 'Time') {
-    $TrgTime = (Get-Date).AddMinutes($Minutes).AddSeconds($Seconds) # Calculate target time if time was specified in minutes
+Write-Verbose $PSCmdlet.ParameterSetName
+Switch ($PSCmdlet.ParameterSetName) {
+    'Time' { [DateTime]$TrgTime = (Get-Date).AddMinutes($Minutes).AddSeconds($Seconds) } # Calculate target time if time was specified in minutes
+    'xPast' {
+        $Now = Get-Date
+        [DateTime]$TrgTime = $Now.AddMinutes(-$Now.Minute).AddSeconds(-$Now.Second).AddMinutes($Minutes).AddMinutes($Seconds)
+        if ($TrgTime -lt $Now) { $TrgTime = $TrgTime.AddHours(1) }
+    }
+    'Target' { [DateTime]$TrgTime = [DateTime]$TrgTime }
 }
 
 # Kill any other running instance of this script
@@ -45,11 +73,8 @@ Get-CimInstance Win32_Process -Filter "name = 'powershell.exe'" | where {($_.Pro
 Write-Host "Counting down until $TrgTime"
 
 do {
-    $CurTime = (Get-Date)
-    $WaitTime = $CurTime.AddMilliseconds(1000-$CurTime.Millisecond)                  # Add one second, but take into account script processing time, rather than lazily adding one second
-    Start-Sleep -Milliseconds ($WaitTime - $CurTime).Milliseconds                    # Wait a bit
-    $Remaining = ($TrgTime - $WaitTime)                                              # Calculate time remaining
-    "$Prefix$($Remaining.ToString($Format))$Suffix" | Out-File $File -Encoding ascii # Write to file
+    "$Prefix$(($TrgTime - (Get-Date)).ToString($Format))$Suffix" | Out-File $File -Encoding ascii # Write to file
+    Start-Sleep -Milliseconds (1000 - (Get-Date).Millisecond)
 } until ($WaitTime -gt $TrgTime)                                                     # Stop once we reach target time
 
 $Done | Out-File $File -Encoding ascii # Write final text
